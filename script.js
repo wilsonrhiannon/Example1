913f7837cd463dbe9615e209609a5032199aa1d1
wilsonrhiannon <60984517+wilsonrhiannon@users.noreply.github.com>
2020-03-25 19:30:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var array = [10,9,"eight",7];
+
+for (var i=10; i>=0; i--){
+    console.log(i);
+}
+console.log("boom!");
+
+
+print ();
+print ();
+
+function print (){
+    console.log("- - - -")
+    console.log("Hello World");
+
+
+
+
+
+
+function myName (){
+    var name = prompt("Username GIVE IT TO ME");
+    return name;
+}
+
+console.log(myName());
+
+function sum(number1, number2){
+    var
+    return number1 + number2;
+
+}
+
+
+function movieDetails(title,released){
+    return("this is a great " + title + " and it was " + released);
+ }
+ movie1=movieDetails();
+
+ console.log(movie1);
+
+ function shoppingCart (item, cost), (balance), (return){
+     if ("item, cost > balance")
+ }
